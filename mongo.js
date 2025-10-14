da3fccc7aaa37e12c55469fdd393e18e7abd4d3d
Felipe <felipe@domingues.dev>
2025-10-14 02:21:04 -0300

    Login system added

--- mongo.js
+++ mongo.js
@@ -8,19 +8,19 @@ module.exports = async function () {
     if (d){
       console.log("Database connected!");
       return d;
-    }
+    };
   } catch (err) {
     console.log("Erro:", err);
-  }
+  };
 };
 
 module.exports.include = async function (name, email, password) {
   const register = new users({ name: name, email: email, password: password });
   await register.save();
-}
+};
 
 module.exports.getEmail = async function (email) {
   return await users.findOne({ email: email });
-}
+};
 
 // include('Haruki', 'hk@test.com', '1234');
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const { getEmail, include } = require('../mongo')
+const { getEmail, include } = require('../mongo');
 
-require('../mongo')()
+require('../mongo')();
 
 const router = express.Router();
 
@@ -12,7 +12,7 @@ router.post('/register', async (req, res) => {
     return res.render('register', { message: "Passwords are different"});
   } else if (!name || !email || !password || !passwordConfirm) {
     return res.render('register', { message: "Place all informations"});
-  }
+  };
 
   const emailExists = await getEmail(email);
 
@@ -25,4 +25,24 @@ router.post('/register', async (req, res) => {
   return res.render('register', { message: "Sucess!"}); 
 });
 
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.render('login', { message: "Place all informations!"});
+  };
+
+  const emailExists = await getEmail(email);
+
+  if (!emailExists) {
+    return res.render('login', { message: "This email does'nt exist."});
+  };
+
+  const compar = await bcrypt.compare(password, emailExists.password);
+  if (!compar) {
+    return res.render('login', { message: "Invalid password!"});
+  }
+
+  return res.render('login', { message: `Logged with ${emailExists.name}`});
+});
+ 
 module.exports = router;
